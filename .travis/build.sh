d3da621f7d9c8ab0ab643c79d2d34d965f27c8ed
Rodrigo Seidi Watanabe <rseidi00@gmail.com>
2016-02-12 02:09:28 -0200

    Update build.sh

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
-npm install grunt-cli -g
-npm install
-
 VERSION=$(xmllint --xpath "//*[local-name()='project']/*[local-name()='version']/text()" pom.xml)
 echo "Detected version: $VERSION"
 
-if ! [ $VERSION=~*SNAPSHOT* ]; then
-	echo "Building RELEASE"
+npm install grunt-cli -g
+npm install
+grunt 
+
+if ![ $VERSION=~*SNAPSHOT* ]; then
+	echo "Deploying Webjar"
 	mvn deploy --settings .travis/maven-settings.xml
 fi
